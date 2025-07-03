e02d9cdf7ffbcb0f68a8012e54179181f58f00d1
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-02 21:57:54 -0700

    Time: 15 ms (15.26%), Space: 25.7 MB (47.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         while(k > len(nums)):
-            k = k - len(nums)
+            k = k % len(nums)
             
         temp = []
         i = 0
