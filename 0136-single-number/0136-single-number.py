6f3fd0df34343e350377f1c7139651a2e13193a7
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-03 23:19:10 -0700

    Time: 2 ms (70.29%), Space: 19.6 MB (77.32%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        numMap = {}
+        xor = 0
         for num in nums:
-            numMap[num] = 1 + numMap.get(num, 0)
-        for num, freq in numMap.items():
-            if(freq == 1):
-                return num
+            xor = xor ^ num
+        return xor
