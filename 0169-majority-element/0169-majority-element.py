3ed01e4f442454927e0e56d373b323072e5cca85
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-05 22:55:51 -0700

    Time: 8 ms (38.07%), Space: 19.5 MB (19.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numMap = {}
+        res = 0
+        count = 0
         for num in nums:
-            numMap[num] = 1 + numMap.get(num, 0)
-
-        for num, freq in numMap.items():
-            if(freq > len(nums)//2):
-                return num
+            if(count == 0):
+                count = 1
+                res = num
+            else:
+                if(num == res):
+                    count += 1
+                else:
+                    count -= 1
+        return res
