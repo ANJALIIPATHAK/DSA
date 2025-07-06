f64a0dc9eac39df4222a0c89ee2f13e4a1eef9e2
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-05 19:04:56 -0700

    Time: 0 ms (100%), Space: 17.8 MB (33.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,9 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(0, len(nums)):
-            j = i
-            while(j > 0 and nums[j] < nums[j-1]):
-                nums[j], nums[j - 1] = nums[j - 1], nums[j]
-                j -= 1
+        def merge(nums, low, mid, high):
+            left = low
+            right = mid + 1
+            temp = []
+            while(left <= mid and right <= high):
+                if(nums[left] <= nums[right]):
+                    temp.append(nums[left])
+                    left += 1
+                else:
+                    temp.append(nums[right])
+                    right += 1
+            while(left <= mid):
+                temp.append(nums[left])
+                left += 1
+            while(right <= high):
+                temp.append(nums[right])
+                right += 1
+            for i in range(low, high + 1):
+                nums[i] = temp[i-low]
+        def helper(low, high):
+            if(low >= high):
+                return
+            mid = (low + high) // 2
+            helper(low, mid)
+            helper(mid + 1, high)
+            merge(nums, low, mid, high)
+        
+        helper(0, len(nums)-1)
+
         
