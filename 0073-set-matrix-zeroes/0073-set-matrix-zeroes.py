0d8c075ae7bba4704ed80fd84c3252a215f0c222
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-11 16:29:03 -0700

    Time: 7 ms (18.78%), Space: 18.4 MB (77.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,16 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def markRow(row):
-            for col in range(0, len(matrix[row])):
-                if(matrix[row][col] != 0):
-                    matrix[row][col] = '*'
-
-        def markCol(col):
-            for row in range(0, len(matrix)):
-                if(matrix[row][col] != 0):
-                    matrix[row][col] = '*'
+        rowMarker = [0]*len(matrix)
+        colMarker = [0]*len(matrix[0])
 
         for row in range(0, len(matrix)):
             for col in range(0, len(matrix[row])):
                 if(matrix[row][col] == 0):
-                    markRow(row)
-                    markCol(col)
+                    rowMarker[row] = 1
+                    colMarker[col] = 1
 
         for row in range(0, len(matrix)):
             for col in range(0, len(matrix[row])):
-                if(matrix[row][col] == '*'):
+                if(rowMarker[row] == 1 or colMarker[col] == 1):
                     matrix[row][col] = 0
