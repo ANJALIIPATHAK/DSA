bf135e5c1a18bb40d0ad199e8d91d564a46fb8b4
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-11 17:26:28 -0700

    Time: 4 ms (38.64%), Space: 18.3 MB (95.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,16 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowMarker = [0]*len(matrix)
-        colMarker = [0]*len(matrix[0])
-
+        col0 = 1
         for row in range(0, len(matrix)):
             for col in range(0, len(matrix[row])):
                 if(matrix[row][col] == 0):
-                    rowMarker[row] = 1
-                    colMarker[col] = 1
+                    matrix[row][0] = 0
+                    if(col != 0):
+                        matrix[0][col] = 0
+                    else:
+                        col0 = 0
 
-        for row in range(0, len(matrix)):
-            for col in range(0, len(matrix[row])):
-                if(rowMarker[row] == 1 or colMarker[col] == 1):
-                    matrix[row][col] = 0
+        for row in range(1, len(matrix)):
+            for col in range(1, len(matrix[row])):
+                if(matrix[row][col] != 0):
+                    if(matrix[0][col] == 0 or matrix[row][0] == 0):
+                        matrix[row][col] = 0
+
+        if(matrix[0][0] == 0):
+            for col in range(0, len(matrix[0])):
+                matrix[0][col] = 0
+
+        if(col0 == 0):
+            for row in range(0, len(matrix)):
+                matrix[row][0] = 0
