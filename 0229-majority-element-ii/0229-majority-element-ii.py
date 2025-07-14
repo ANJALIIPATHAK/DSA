504b8a5451fb44b6a1bf95ec788f7f66b2a3f37c
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-14 14:13:35 -0700

    Time: 10 ms (35.9%), Space: 22.4 MB (36.49%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,9 +2,10 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         res = [ ]
         numMap = {}
+        n = len(nums)
         for num in nums:
             numMap[num] = 1 + numMap.get(num, 0)
-            if(numMap[num] == len(nums)//3 + 1):
+            if(numMap[num] == n//3 + 1):
                 res.append(num)
             if(len(res) == 2):
                 return res
