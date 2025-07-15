2ef4f2ca0455284a8c86c1bbb6aa0299ef9f8301
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-14 23:14:10 -0700

    Time: 380 ms (67.09%), Space: 18 MB (15.86%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,14 +1,28 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        finalRes = set()
+        nums = sorted(nums)
+        finalRes = []
         for i in range(0, len(nums)):
+            if(i > 0 and nums[i] == nums[i-1]):
+                continue
             for j in range(i + 1, len(nums)):
-                numMap = {}
-                for k in range(j + 1, len(nums)):
-                    l = target - (nums[i] + nums[j] + nums[k])
-                    if(l in numMap):
-                        res = [nums[i], nums[j], nums[k], l]
-                        res = tuple(sorted(res))
-                        finalRes.add(res)
-                    numMap[nums[k]] = 1
-        return list(finalRes)
+                if(j > i + 1 and nums[j] == nums[j-1]):
+                    continue
+                k = j + 1
+                l = len(nums)-1
+                while(k < l):
+                    sum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if(sum < target):
+                        k += 1
+                    elif(sum > target):
+                        l -= 1
+                    elif(sum == target):
+                        finalRes.append([nums[i], nums[j], nums[k], nums[l]])
+                        k += 1
+                        l -= 1
+                        while(k < l and nums[k] == nums[k-1]):
+                            k += 1
+                        while(k < l and nums[l] == nums[l+1]):
+                            l -= 1
+        return finalRes
+                    
