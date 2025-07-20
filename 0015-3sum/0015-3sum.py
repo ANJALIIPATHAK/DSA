e7651f0d0a41618f554af0f78d547a8788642345
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-19 19:37:24 -0700

    Time: 554 ms (70.54%), Space: 20.8 MB (36.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,24 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        count = defaultdict(int)
-        for num in nums:
-            count[num] += 1
-
         res = []
-        for i in range(len(nums)):
-            count[nums[i]] -= 1
-            if i and nums[i] == nums[i - 1]:
+        nums = sorted(nums)
+        for i in range(0, len(nums)):
+            if(i > 0 and nums[i] == nums[i-1]):
                 continue
-                
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] -= 1
-                if j - 1 > i and nums[j] == nums[j - 1]:
-                    continue
-                target = -(nums[i] + nums[j])
-                if count[target] > 0:
-                    res.append([nums[i], nums[j], target])
-
-            for j in range(i + 1, len(nums)):
-                count[nums[j]] += 1
+            left = i + 1
+            right = len(nums)-1
+            while(left < right):
+                sum = nums[i] + nums[left] + nums[right]
+                if(sum > 0):
+                    right -= 1
+                elif(sum < 0):
+                    left += 1
+                elif(sum == 0):
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while(left < right and nums[left] == nums[left-1]):
+                        left += 1
+                    while(left < right and nums[right] == nums[right + 1]):
+                        right -= 1
         return res
+
+
