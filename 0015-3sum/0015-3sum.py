af3d435aac58d872b9a70889b5cbad3a3e359c2a
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-19 19:08:06 -0700

    Time: 2558 ms (5%), Space: 61.5 MB (5.17%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        finalRes = []
-        nums = sorted(nums)
-        i = 0 
-        for i in range(0, len(nums)):
-            if(i > 0 and nums[i] == nums[i-1]):
+        nums.sort()
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+
+        res = []
+        for i in range(len(nums)):
+            count[nums[i]] -= 1
+            if i and nums[i] == nums[i - 1]:
                 continue
-            j = i + 1
-            k = len(nums)-1
-            while(j < k):
-                sum = nums[i] + nums[j] + nums[k]
-                if(sum < 0):
-                        j += 1
-                elif(sum > 0):
-                        k -= 1
-                elif(sum == 0):
-                    finalRes.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    k -= 1
-                    while(j < k and nums[j] == nums[j - 1]):
-                        j += 1
-                    while(j < k and nums[k] == nums[k + 1]):
-                        k -= 1
-        return finalRes
+                
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] -= 1
+                if j - 1 > i and nums[j] == nums[j - 1]:
+                    continue
+                target = -(nums[i] + nums[j])
+                if count[target] > 0:
+                    res.append([nums[i], nums[j], target])
+
+            for j in range(i + 1, len(nums)):
+                count[nums[j]] += 1
+        return res
