12597818fb31adf0477a4e7037b563346d43023d
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-07-21 11:40:05 -0700

    Time: 0 ms (100%), Space: 23.3 MB (81.25%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,31 +8,37 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        #Finding Middle of List
         slow = head
         fast = head.next
 
         while(fast and fast.next):
             slow = slow.next
             fast = fast.next.next
+        second = slow.next  # This is the beginning of the second half
+        slow.next = None    #Breaking the link between first half and second half lists
 
-        second = slow.next
-        slow.next = None
-        
+        #Reversing second half:
         prev = None
         while(second):
             temp = second.next
             second.next = prev
             prev = second
             second = temp
+        second = prev   #This is the new head of the reversed second half list
 
-        second = prev
+        #Merging first and second half
         first = head
 
         while(second):
-            tempFirst = first.next
-            tempSecond = second.next
+            temp1 = first.next
+            temp2 = second.next
             first.next = second
-            second.next = tempFirst
-            first = tempFirst
-            second = tempSecond
+            second.next = temp1
+            first = temp1
+            second = temp2
+
+
+
+
 
