254a046fc55852eb51a4a03f0b7ed0f96718e44d
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-09-02 23:19:07 -0700

    Time: 2 ms (53.41%), Space: 18.7 MB (61.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,7 +5,7 @@ class Solution:
         for i in range(0, len(nums)):
             numMap[nums[i]] = i
 
-        for i in range(0, len(nums)):
+        for i in range(0, len( nums)):
             diff = target - nums[i]
-            if(diff in numMap and i != numMap[diff]):
+            if diff in numMap and i != numMap[diff]:
                 return [i, numMap[diff]]
