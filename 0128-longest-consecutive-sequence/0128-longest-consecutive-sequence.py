c118d1772fc44f313f52119287f8cd55d7e10056
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-10-27 14:57:18 -0700

    Time: 59 ms (33.56%), Space: 34.3 MB (29.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -4,11 +4,10 @@ class Solution:
         maxCount = 0
 
         for num in numSet:
-            count = 1
             if (num - 1) not in numSet:
+                count = 1
                 while((num + 1) in numSet):
                     count += 1
-                    num = num + 1
+                    num += 1
                 maxCount = max(maxCount, count)
         return maxCount
-        
