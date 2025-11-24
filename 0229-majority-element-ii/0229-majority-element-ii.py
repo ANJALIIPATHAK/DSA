567c252a50a68a8fbaabbffb7e3c54ab9641e6ce
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-11-23 23:15:03 -0800

    Time: 7 ms (62.65%), Space: 22.1 MB (52.61%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,35 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        count1 = count2 = 0
-        elem1 = elem2 = -10**-9 - 1
+        numMap = {}
+        n = len(nums)
 
         for num in nums:
-            if(count1 == 0 and num != elem2):
-                count1 = 1
-                elem1 = num
-            elif(count2 == 0 and num != elem1):
-                count2 = 1
-                elem2 = num
-            elif(num == elem1):
-                count1 +=1
-            elif(num == elem2):
-                count2 += 1
-            else:
-                count1 -= 1
-                count2 -= 1
-
-        count1 = count2 = 0
-        for num in nums:
-            if(num == elem1):
-                count1 += 1
-            if(num == elem2):
-                count2 += 1
+            numMap[num] = 1 + numMap.get(num, 0)
 
         res = []
-        min = (len(nums) // 3 + 1)
-        if(count1 >= min):
-            res.append(elem1)
-        if(count2 >= min):
-            res.append(elem2)
+        for num, count in numMap.items():
+            if count > n/3:
+                res.append(num)
+        
         return res
 
