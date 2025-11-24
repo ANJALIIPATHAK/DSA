205a078eb2102ed3aadfe64c3ebe614bcdbb7400
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-11-23 23:36:01 -0800

    Time: 15 ms (10.63%), Space: 20.6 MB (60.99%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,15 +1,22 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        numMap = {}
-        n = len(nums)
+        numMap = defaultdict(int)
 
         for num in nums:
-            numMap[num] = 1 + numMap.get(num, 0)
+            numMap[num] += 1
+            
+            if len(numMap) <= 2:
+                continue
+            
+            newNumMap = defaultdict(int)
+            for num, count in numMap.items():
+                if count > 1:
+                    newNumMap[num] = count-1
+            numMap = newNumMap
 
         res = []
-        for num, count in numMap.items():
-            if count > n/3:
+        for num in numMap:
+            if nums.count(num) > len(nums)/3:
                 res.append(num)
-        
         return res
 
