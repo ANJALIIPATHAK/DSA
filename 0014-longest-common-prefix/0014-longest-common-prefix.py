8b82c1aa887610fbb692fe5b41c49f951f600c01
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-12-09 13:06:15 -0800

    Time: 0 ms (100%), Space: 17.9 MB (29.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,8 +3,8 @@ class Solution:
         res = ""
 
         for i in range(0, len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
+            for word in strs:
+                if i == len(word) or word[i] != strs[0][i]:
                     return res
             res += strs[0][i]
         return res
