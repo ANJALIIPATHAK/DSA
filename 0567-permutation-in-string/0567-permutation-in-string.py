90b5f349d2a70b0911280455cc4e1c446ae43371
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-12-18 15:37:04 -0800

    Time: 11 ms (97.1%), Space: 17.4 MB (99.98%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,23 +3,23 @@ class Solution:
         m = len(s1)
         n = len(s2)
 
-        if(m > n):
+        if m > n:
             return False
-        
-        count1 = [0]*26
-        count2 = [0]*26
+
+        count1 = [0] * 26
+        count2 = [0] * 26
 
         for i in range(0, m):
             count1[ord(s1[i]) - ord("a")] += 1
             count2[ord(s2[i]) - ord("a")] += 1
 
-        if(count1 == count2):
+        if count1 == count2:
             return True
-        
+
         for i in range(m, n):
             count2[ord(s2[i]) - ord("a")] += 1
-            count2[ord(s2[i-m]) - ord("a")] -= 1
-            if(count1 == count2):
+            count2[ord(s2[i - m]) - ord("a")] -= 1
+            if count1 == count2:
                 return True
         return False
 
