7f03170c37849daf6c2cf33a3093aa8816165849
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2025-12-19 12:14:32 -0800

    Time: 66 ms (71.61%), Space: 17.9 MB (99.77%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,35 +1,39 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if(t == ""):
+        if t == "":
             return ""
-        
+
         countT = {}
+        window = {}
+
         for char in t:
             countT[char] = 1 + countT.get(char, 0)
-        need = len(countT)
-        
-        countWindow = {}
 
+        need = len(countT)
         have = 0
+
         left = 0
         res = [-1, -1]
         resLen = float("inf")
+
         for right in range(0, len(s)):
-            countWindow[s[right]] = 1 + countWindow.get(s[right], 0)
-            if(s[right] in countT and countWindow[s[right]] == countT[s[right]]):
+            char = s[right]
+            window[char] = 1 + window.get(char, 0)
+
+            if char in countT and window[char] == countT[char]:
                 have += 1
+
             while(have == need):
-                if(resLen > right - left + 1):
+                if right - left + 1 < resLen:
                     res = [left, right]
                     resLen = right - left + 1
-                countWindow[s[left]] -= 1
-                if(s[left] in countT and countWindow[s[left]] < countT[s[left]]):
+
+                window[s[left]] -= 1
+                if s[left] in countT and window[s[left]] < countT[s[left]]:
                     have -= 1
                 left += 1
-        left = res[0]
-        right = res[1]
-        if(resLen != float("inf")):
-            return s[left : right + 1]
-        else:
-            return ""
-                
+        
+        left, right = res
+        return s[left : right + 1] if resLen != float("inf") else ""
+
+         
