60472faa6a6d4763ad92736810a20db88bc37f91
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-04-25 18:43:38 -0700

    Time: 7 ms (50.55%), Space: 24.7 MB (20.31%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,18 +4,20 @@ class Solution:
         for num in nums:
             numMap[num] = 1 + numMap.get(num, 0)
 
-        countArr = []
+        countArr = [[] for i in range(0, len(nums) + 1)]
 
         for num, count in numMap.items():
-            countArr.append([count, num])
-        
-        countArr.sort()
+            countArr[count].append(num)
 
         res = []
-        while (len(res) < k):
-            res.append(countArr.pop()[1])
-        
-        return res
+        for i in range(len(countArr) - 1, 0, -1):
+            for num in countArr[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+
+
+
 
 
         
