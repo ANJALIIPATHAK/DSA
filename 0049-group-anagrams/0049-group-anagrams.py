0ac1c347be16777b3b7236cd4a2114514be5553d
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-05-04 22:46:45 -0700

    Time: 18 ms (24.42%), Space: 24.1 MB (9.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,7 +3,7 @@ class Solution:
         strMap = defaultdict(list)
 
         for word in strs:
-            charArr = [0] * 26
+            charArr = [0]*26
             for char in word:
                 charArr[ord(char) - ord("a")] += 1
             strMap[tuple(charArr)].append(word)
