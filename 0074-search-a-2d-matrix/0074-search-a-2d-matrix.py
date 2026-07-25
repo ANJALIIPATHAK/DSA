1ece576068b8726eff8dc2187aa7b236b2672740
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 14:28:16 -0700

    Time: 0 ms (100%), Space: 19.5 MB (77.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,25 +5,26 @@ class Solution:
 
         topRow = 0
         bottomRow = rows - 1
+
         while(topRow <= bottomRow):
-            midRow = (topRow + bottomRow)//2
-            if(target > matrix[midRow][-1]):
+            midRow = (topRow + bottomRow) // 2
+            if target > matrix[midRow][-1]:
                 topRow = midRow + 1
-            elif(target < matrix[midRow][0]):
+            elif target < matrix[midRow][0]:
                 bottomRow = midRow - 1
             else:
                 break
-
-        left = 0
-        right = len(matrix[midRow])-1
+        
         finalRow = matrix[midRow]
-        while(left <= right):
-            mid = (left + right)//2
-            if(finalRow[mid] > target):
-                right = mid - 1
-            elif(finalRow[mid] < target):
-                left = mid + 1
-            elif(finalRow[mid] == target):
-                return True
-        return False
+        left = 0
+        right = len(finalRow) - 1
 
+        while(left <= right):
+            mid = (left + right) // 2
+            if finalRow[mid] == target:
+                return True
+            elif finalRow[mid] > target:
+                right = mid - 1
+            elif finalRow[mid] < target:
+                left = mid + 1
+        return False
