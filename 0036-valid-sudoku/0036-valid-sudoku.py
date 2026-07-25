e7cfdfd03d33008054f8b2aca37f88c0cc577705
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 11:45:37 -0700

    Time: 4 ms (47.29%), Space: 19.2 MB (94.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        squares = defaultdict(set)
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        squares = defaultdict(list)
 
         for row in range(0, 9):
             for col in range(0, 9):
@@ -10,7 +10,7 @@ class Solution:
                     continue
                 if board[row][col] in rows[row] or board[row][col] in cols[col] or board[row][col] in squares[(row//3, col//3)]:
                     return False
-                rows[row].add(board[row][col])
-                cols[col].add(board[row][col])
-                squares[(row//3, col//3)].add(board[row][col])
+                rows[row].append(board[row][col])
+                cols[col].append(board[row][col])
+                squares[(row//3, col//3)].append(board[row][col])
         return True
