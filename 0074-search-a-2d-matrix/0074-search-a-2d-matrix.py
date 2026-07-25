568916edd4ac02726aee75a3382a83c9c5d6238d
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 15:56:06 -0700

    Time: 0 ms (100%), Space: 19.5 MB (38.02%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,7 +5,6 @@ class Solution:
 
         topRow = 0
         bottomRow = rows - 1
-
         while(topRow <= bottomRow):
             midRow = (topRow + bottomRow) // 2
             if target > matrix[midRow][-1]:
@@ -18,13 +17,12 @@ class Solution:
         finalRow = matrix[midRow]
         left = 0
         right = len(finalRow) - 1
-
         while(left <= right):
             mid = (left + right) // 2
-            if finalRow[mid] == target:
-                return True
-            elif finalRow[mid] > target:
-                right = mid - 1
-            elif finalRow[mid] < target:
+            if target > finalRow[mid]:
                 left = mid + 1
+            elif target < finalRow[mid]:
+                right -= 1
+            else:
+                return True
         return False
