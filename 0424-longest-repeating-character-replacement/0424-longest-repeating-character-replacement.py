11cc44a2cbc3b003cf0824c24983fedaa36205d1
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 14:14:57 -0700

    Time: 143 ms (28.3%), Space: 19.6 MB (84.93%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,7 +3,6 @@ class Solution:
         maxLength = 0
         charMap = {}
         left = 0
-
         for right in range(0, len(s)):
             charMap[s[right]] = 1 + charMap.get(s[right], 0)
             while((right - left + 1) - max(charMap.values()) > k):
