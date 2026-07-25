81737159b2c4809e52ce3c8b7a1ab6dfddb3be81
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 15:03:30 -0700

    Time: 19 ms (19.65%), Space: 23.9 MB (26.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         strMap = defaultdict(list)
+
         for word in strs:
             charArr = [0] * 26
             for char in word:
