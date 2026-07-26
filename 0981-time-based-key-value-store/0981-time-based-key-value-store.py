4efc93cebdefce26791541b87b8f21227ef4f069
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 21:34:17 -0700

    Time: 144 ms (38.25%), Space: 69.6 MB (29.4%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -11,16 +11,15 @@ class TimeMap:
         values = self.timeMap.get(key, [])
         left = 0
         right = len(values) - 1
-
         while(left <= right):
             mid = (left + right) // 2
             if values[mid][1] == timestamp:
                 res = values[mid][0]
-                return res
+                break
             elif values[mid][1] < timestamp:
                 res = values[mid][0]
                 left = mid + 1
-            else:
+            elif values[mid][1] > timestamp:
                 right = mid - 1
         return res
 
