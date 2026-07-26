c79b9fc134297b484ef065912ee67712c6e27756
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-26 00:43:57 -0700

    Time: 67 ms (5.9%), Space: 22.4 MB (86.14%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -9,7 +9,7 @@ class Solution:
         slow = head
         fast = head
 
-        while(fast and fast.next):
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
             if slow == fast:
