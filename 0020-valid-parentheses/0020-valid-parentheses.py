853a9e469cd3ed5762085d154159b1a106be7436
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-25 17:04:49 -0700

    Time: 0 ms (100%), Space: 19.2 MB (91.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,6 +1,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        charMap = { ")" : "(", "}" : "{", "]" : "["}
+        charMap = {')' : '(', '}' : '{', ']' : '['}
         stack = []
 
         for char in s:
@@ -12,3 +12,5 @@ class Solution:
             else:
                 stack.append(char)
         return True if not stack else False
+
+
