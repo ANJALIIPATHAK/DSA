1534c815cb897a3e175b89d0fbf4693bb85c403c
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-27 18:27:05 -0700

    Time: 11 ms (31.77%), Space: 19.5 MB (93.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1
+
         while(left < right):
             while(left < right and not s[left].isalnum()):
                 left += 1
