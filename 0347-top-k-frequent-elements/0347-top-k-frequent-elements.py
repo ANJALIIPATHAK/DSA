53178cea41780a06b6c794f575006cd13c50666b
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-27 18:13:29 -0700

    Time: 11 ms (25.22%), Space: 24.6 MB (26.65%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,7 +3,7 @@ class Solution:
         numMap = {}
         for num in nums:
             numMap[num] = 1 + numMap.get(num, 0)
-
+        
         countArr = [[] for i in range(0, len(nums) + 1)]
         for num, count in numMap.items():
             countArr[count].append(num)
@@ -14,4 +14,3 @@ class Solution:
                 res.append(num)
                 if len(res) == k:
                     return res
-        
