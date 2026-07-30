81258e9908f77ca167347d495818a4d7b321b17d
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-29 21:37:09 -0700

    Time: 0 ms (100%), Space: 20.6 MB (9.39%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,15 +6,19 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(root):
-            if(not root):
-                return [True, 0]
+        res = True
+        def dfs(node):
+            nonlocal res
+            if not node: 
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            if abs(left - right) > 1: 
+                res = False
+            return 1 + max(left, right)
+        dfs(root)
+        return res
 
-            left = dfs(root.left)
-            right = dfs(root.right)
+    
 
-            balanced = (left[0]) and (right[0]) and (abs(left[1] - right[1]) <= 1)
-
-            return [balanced, 1 + max(left[1], right[1])]
-
-        return dfs(root)[0]
+            
