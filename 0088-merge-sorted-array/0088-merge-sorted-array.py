a9b21f4133b4e7e1b3475e4f96d8870c8f4690e5
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-29 20:24:32 -0700

    Time: 0 ms (100%), Space: 19.4 MB (8.75%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,7 +7,7 @@ class Solution:
         p1 = m - 1
         p2 = n - 1
 
-        while(p1 >= 0 and p2 >= 0):
+        while(p1 >=0 and p2 >= 0):
             if nums1[p1] > nums2[p2]:
                 nums1[write] = nums1[p1]
                 p1 -= 1
@@ -15,6 +15,7 @@ class Solution:
                 nums1[write] = nums2[p2]
                 p2 -= 1
             write -= 1
+        
         while(p2 >= 0):
             nums1[write] = nums2[p2]
             p2 -= 1
