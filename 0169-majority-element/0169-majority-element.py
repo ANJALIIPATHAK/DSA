bd2e80d7ac57be6d28f2adba33d833b861e0bbf5
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-07-29 20:55:38 -0700

    Time: 7 ms (57.21%), Space: 21.1 MB (48.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numMap = {}
-        for num in nums:
-            numMap[num] = 1 + numMap.get(num, 0)
+        candidate = None
+        count = 0
 
-        for num in numMap:
-            if numMap[num] > len(nums) / 2:
-                return num
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
