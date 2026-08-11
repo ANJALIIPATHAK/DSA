f3f0bd74530ac9b61dc5f7ec95a67c0fb6cfd156
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-08-11 16:22:21 -0700

    Time: 0 ms (100%), Space: 19.4 MB (18.64%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,18 +1,18 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        i = 0
+        left = 0
         target = 0
 
-        while(i < len(chars)):
-            chars[target] = chars[i]
+        while(left < len(chars)):
+            chars[target] = chars[left]
             target += 1
-            j = i + 1
-            while(j < len(chars) and chars[i] == chars[j]):
-                j += 1
-            if j - i > 1:
-                groupLength = str(j - i)
+            right = left + 1
+            while(right < len(chars) and chars[left] == chars[right]):
+                right += 1
+            if right - left > 1:
+                groupLength = str(right - left)
                 for char in groupLength:
                     chars[target] = char
                     target += 1
-            i = j
+            left = right
         return target
