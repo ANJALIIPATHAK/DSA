7c8cf6327de19a0e0318fa7a19b0786e8cd3c242
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-08-11 15:59:03 -0700

    Time: 0 ms (100%), Space: 19.4 MB (55.6%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -7,15 +7,12 @@ class Solution:
             chars[target] = chars[i]
             target += 1
             j = i + 1
-
-            while j < len(chars) and chars[i] == chars[j]:
+            while(j < len(chars) and chars[i] == chars[j]):
                 j += 1
-
             if j - i > 1:
-                for char in str(j - i):
+                groupLength = str(j - i)
+                for char in groupLength:
                     chars[target] = char
                     target += 1
-
             i = j
-
         return target
