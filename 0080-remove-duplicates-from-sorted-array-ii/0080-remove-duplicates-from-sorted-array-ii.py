852a99ddd228cba2b6dda6e997d6ecbf893466cd
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-08-11 20:22:15 -0700

    Time: 93 ms (24.94%), Space: 22.3 MB (46.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -3,13 +3,14 @@ class Solution:
         left = 0
         right = 0
 
-        while(right < len(nums)):
+        while right < len(nums):
             count = 1
-            while(right + 1 < len(nums) and nums[right] == nums[right + 1]):
-                count += 1
+            while right + 1 < len(nums) and nums[right] == nums[right + 1]:
                 right += 1
+                count += 1
             for i in range(min(2, count)):
                 nums[left] = nums[right]
                 left += 1
             right += 1
         return left
+            
