d21addbb6c8328a72574206dc49484d479d053be
ANJALIIPATHAK <95901942+ANJALIIPATHAK@users.noreply.github.com>
2026-08-11 22:48:59 -0700

    Time: 385 ms (75.14%), Space: 19.1 MB (97.08%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -3,25 +3,27 @@ class Solution:
         res = []
         nums = sorted(nums)
         for i in range(0, len(nums)):
-            if(i > 0 and nums[i] == nums[i - 1]):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i + 1, len(nums)):
-                if(j > i + 1 and nums[j] == nums[j - 1]):
+                if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
                 left = j + 1
                 right = len(nums) - 1
-                while(left < right):
+                while left < right:
                     sum = nums[i] + nums[j] + nums[left] + nums[right]
-                    if(sum < target):
-                        left += 1
-                    elif(sum > target):
+                    if sum > target:
                         right -= 1
+                    elif sum < target:
+                        left += 1
                     else:
                         res.append([nums[i], nums[j], nums[left], nums[right]])
                         left += 1
                         right -= 1
-                        while(left < right and nums[left] == nums[left - 1]):
+                        while left < right and nums[left] == nums[left - 1]:
                             left += 1
-                        while(left < right and nums[right] == nums[right + 1]):
+                        while left < right and nums[right] == nums[right + 1]:
                             right -= 1
         return res
+
+        
